8ee6ad9fd655bdaa0b06246c1d128bc67721f73d
DmitryLT <dim.sha@yandex.ru>
2023-09-16 15:58:25 +0300

    first commit

---
+++ src/DependencyInjection/TelegramBotSenderExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ltdsh\TelegramBotSender\DependencyInjection;
+
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class TelegramBotSenderExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
---
+++ src/TelegramBotSender.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ltdsh\TelegramBotSender;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TelegramBotSender extends Bundle
+{
+    public function sendSimpleMessage($token, $chatId)
+    {
+        $getQuery = array(
+            "chat_id" 	=> $chatId,
+            "text"  	=> "Новое сообщение из формы бандла",
+            "parse_mode" => "html",
+        );
+        $ch = curl_init("https://api.telegram.org/bot". $token ."/sendMessage?" . http_build_query($getQuery));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+
+        $resultQuery = curl_exec($ch);
+        curl_close($ch);
+
+        return $resultQuery;
+    }
+}
